795e74de11dd4b391f93da86fdb176707a1507b7
Lorenz <lorenz@mintert.org>
2013-02-19 10:59:12 +0100

    fix

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -2,14 +2,11 @@
 if defined?(WillPaginate)
   module WillPaginate
     module ActiveRecord
-  class Relation
-   alias_method :total_count, :count
-     end
-  module RelationMethods
-         alias_method :per, :per_page
-         alias_method :num_pages, :total_pages
-         alias_method :total_count, :count
-  end
+      module RelationMethods
+        alias_method :per, :per_page
+        alias_method :num_pages, :total_pages
+        alias_method :total_count, :count
+      end
     end
   end
 end
