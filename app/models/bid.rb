e87bb5952111d170e93bb12f039b7e72d66ee4e7
Christian Lange <tictac1988ger@gmail.com>
2013-02-19 15:58:39 +0100

    validate bid+

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -7,6 +7,8 @@ class Bid < ActiveRecord::Base
   	:numericality => true,
   	:presence => true
 
+  validates :user_id, :uniqueness => { :scope => :task_id, :message => "only one bid per task" }
+
   def bid_within_range
   	if amount <= 0 || amount > task.budget
   		self.errors[:base] << "Please ensure that your bid is in range for the budget."
