2ba943133170922bba48812c01e7122c96ee2c17
Lorenz <lorenz@mintert.org>
2013-02-27 23:27:14 +0100

    mailer specs

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -1,27 +1,38 @@
 require "spec_helper"
 
 describe "bid emails" do
-#  include ActionController::UrlWriter
-
   before(:all) do
-    @email = Mailer.create_signup("thatsmyway1234@trash-mail.com", "tom jones")
+    reset_email
   end
 
   subject { @email }
   
   it "should send out a bid_accepted email" do
-  	mail()
-    should deliver_to("thatsmyway1234@trash-mail.com")
+    bid = FactoryGirl.create(:bid)
+    task = bid.task
+
+    task.accepted = bid.id
+    task.save
+
+    last_email.to.should include(bid.user.email)
   end
 
   it "should send out a bid_declined email" do
-  	mail()
-    should deliver_to("thatsmyway1234@trash-mail.com")
+    task = FactoryGirl.create(:task)
+    bid1 = FactoryGirl.create(:bid, :task => task)
+    bid2 = FactoryGirl.create(:bid, :task => task)
+
+    task.accepted = bid1.id
+    task.save
+
+    last_email.to.should include(bid2.user.email)
   end
 
   it "should send out a bid_notification email" do
-  	mail()
-    should deliver_to("thatsmyway1234@trash-mail.com")
+    bid = FactoryGirl.create(:bid)
+    email = bid.task.project.user.email
+
+    last_email.to.should include(email)
   end
   
   
